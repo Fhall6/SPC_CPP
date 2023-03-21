c3d493fc2897f5f94300f5c6f2cc02b48938c571
Frank <102326791+Fhall6@users.noreply.github.com>
2023-03-21 17:38:37 -0400

    Add files via upload
    This is the first assignment of the follow up course Advanced C++ Programming (COP2222). It contains content for chapters 7 - 9. 
    
    Left off: Step 4

---
+++ Chapter7_9.cpp
@@ -0,0 +1,80 @@
+/* Frank Hall III
+ * March 21, 2023 ©
+ * chapter 7 & 9 Assignment
+ * 
+ * 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	//Step 1a
+	double dArr[5];
+
+	//Step 1b
+	long lArr[7] = { 100000, 134567, 123456, 9, -234567, -1, 123489 };
+
+	//Step 1c
+	int iArr[3][5];
+
+	//Step 1d
+	char sName[30];
+
+	//Step 2
+	short cnt1, cnt2;
+	long double total;
+
+	//Step 3
+	long highest;
+
+	//Step 4
+	srand((unsigned int)time(NULL));
+	for (cnt1 = 0; cnt)
+	//Step 5
+
+	//Step 6
+
+	//Step 7
+
+	//Step 8
+
+	//Step 9
+
+	//Step 10
+
+	//Step 11
+
+	//Step 12
+
+	//Step 13
+
+	//Step 14
+
+	//Step 15
+
+	//Step 16
+
+	//Step 17
+
+	//Step 18
+
+	//Step 19
+
+	//Step 20
+
+	//Step 21
+
+	//Step 22
+
+	//Step 23
+
+	//Step 24
+
+	//Step 25
+
+}
